cddc913c1ca5acf9fe6cd449490947916ee836c9
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-06-04 02:19:51 +0200

    Resolves Wbondar/techsupport-db#35.

--- test.sh
+++ test.sh
@@ -1,13 +1,12 @@
-source credentials.conf
+#!/bin/sh
 
-echo "Testing basic issue functionality. All SELECT statements are expected to return \"true\"."
-mysql --user=$CLIENT_USERNAME --password=$CLIENT_PASSWORD < tests/test_basic_issue_functionality.sql > logs/test_basic_issue_functionality.log $DATABASE
-cat logs/test_basic_issue_functionality.log
+#Import settings here in order to free test cases from burden locating them afterwards.
+source *.conf
 
-echo "Testing accessiblity of the core of the database schema to the user of name $CLIENT_USERNAME. All statements are expected to fail due to permission violation."
-mysql --user=$CLIENT_USERNAME --password=$CLIENT_PASSWORD < tests/test_accessibility.sql > logs/test_accessibility.log $DATABASE
-cat logs/test_accessibility.log
-
-echo "Testing comment posting on issues."
-mysql --user=$CLIENT_USERNAME --password=$CLIENT_PASSWORD < tests/test_commenting.sql > logs/test_commenting.log $DATABASE
-cat logs/test_commenting.log
+for TEST_CASE_DIRECTORY in `ls -d tests/*`
+do
+    echo 'Executing test case located at '$TEST_CASE_DIRECTORY
+    . $TEST_CASE_DIRECTORY/before.sh
+    . $TEST_CASE_DIRECTORY/run.sh
+    . $TEST_CASE_DIRECTORY/after.sh
+done
--- tests/test_commenting.sql
+++
@@ -1,9 +0,0 @@
-SET @var_author_id = 2;
-SET @var_issue_id = 4;
-CALL comment(@var_issue_id, @var_author_id, 'Content of the first comment.', NULL, @last_comment_id)
-;
-CALL comment(@var_issue_id, @var_author_id, 'Content of the reply to the first comment.', @last_comment_id, @last_comment_id)
-;
-SELECT *
-FROM view_comment
-;
