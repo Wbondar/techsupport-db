67ce3286a810f90fa833d9ec1cede7fa58e65e58
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-06-06 01:49:45 +0200

    Resolves Wbondar/techsupport-db#33.

--- doc.sh
+++ doc.sh
@@ -3,6 +3,6 @@ source credentials.conf
 git checkout gh-pages
 java -jar schemaSpy_5.0.0.jar -t mysql -u $ADMIN_USERNAME -p $ADMIN_PASSWORD -db $DATABASE -host $HOST:$PORT -o . -dp $CONNECTOR_J -hq -norows
 git add *
-git commit -a -m "Documentation updated."
+git commit -a -m "$1"
 git push -u hub gh-pages:gh-pages
 git checkout master
