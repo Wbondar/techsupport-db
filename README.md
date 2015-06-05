c1db685a4d2798977075564583e33249d8209cf0
Vladyslav Bondarenko <vladyslavbond@protonmail.ch>
2015-06-06 01:46:42 +0200

    Resolves Wbondar/techsupport-db#33.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # techsupport-db
 Database schema for issue tracking application.
 
-Documentation can be found at `docs` directory.
+Documentation can be found at GitHub Pages: https://wbondar.github.io/techsupport-db/
--- doc.sh
+++ doc.sh
@@ -1,3 +1,8 @@
 source credentials.conf
 
-java -jar schemaSpy_5.0.0.jar -t mysql -u $ADMIN_USERNAME -p $ADMIN_PASSWORD -db $DATABASE -host $HOST:$PORT -o docs -dp $CONNECTOR_J -hq -norows
+git checkout gh-pages
+java -jar schemaSpy_5.0.0.jar -t mysql -u $ADMIN_USERNAME -p $ADMIN_PASSWORD -db $DATABASE -host $HOST:$PORT -o . -dp $CONNECTOR_J -hq -norows
+git add *
+git commit -a -m "Documentation updated."
+git push -u hub gh-pages:gh-pages
+git checkout master
